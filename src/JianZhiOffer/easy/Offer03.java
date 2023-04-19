698d660e04105ed12c0bdabb4d4546316a4c9dec
ryuyal <helloliu777@163.com>
2023-04-19 12:20:39 +0800

    Create JianZhi Offer03

---
+++ src/JianZhiOffer/easy/Offer03.java
@@ -0,0 +1,8 @@
+package JianZhiOffer.easy;
+
+public class Offer03 {
+    public static void main(String[] args) {
+        System.out.println("This is Jian Zhi Offer 03!");
+        System.out.println("Good Luck!");
+    }
+}
