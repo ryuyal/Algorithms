b3ed2f838ed4008f27d0f76250fc1cd4052f927d
ryuyal <helloliu777@163.com>
2023-04-19 11:11:46 +0800

    Create Hot100-1

---
+++ src/LeetCodeHot100/Hot1to10/Hot1.java
@@ -0,0 +1,8 @@
+package LeetCodeHot100.Hot1to10;
+
+public class Hot1 {
+    public static void main(String[] args) {
+        System.out.println("This is LeetCode Hot100-1!!!");
+        System.out.println("Good Luck!");
+    }
+}
