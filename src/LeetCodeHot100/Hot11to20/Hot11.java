28e0c1c9f2b57e93e2cad212a23057a3b283f90a
ryuyal <helloliu777@163.com>
2023-04-19 11:18:08 +0800

    Create Hot2 and Hot11

---
+++ src/LeetCodeHot100/Hot11to20/Hot11.java
@@ -0,0 +1,8 @@
+package LeetCodeHot100.Hot11to20;
+
+public class Hot11 {
+    public static void main(String[] args) {
+        System.out.println("This is LeetCode Hot100-11!!!");
+        System.out.println("Good Luck!");
+    }
+}
---
+++ src/LeetCodeHot100/Hot1to10/Hot2.java
@@ -0,0 +1,8 @@
+package LeetCodeHot100.Hot1to10;
+
+public class Hot2 {
+    public static void main(String[] args) {
+        System.out.println("This is LeetCode Hot100-2!!!");
+        System.out.println("Good Luck!");
+    }
+}
