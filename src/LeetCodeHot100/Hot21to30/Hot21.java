ec9d2fdb283c2e49597dfc15ef87b4fbac2ef56e
ryuyal <helloliu777@163.com>
2023-04-19 11:21:51 +0800

    Create Hot21

---
+++ src/LeetCodeHot100/Hot21to30/Hot21.java
@@ -0,0 +1,8 @@
+package LeetCodeHot100.Hot21to30;
+
+public class Hot21 {
+    public static void main(String[] args) {
+        System.out.println("This is LeetCode Hot100-21!!!");
+        System.out.println("Good Luck!");
+    }
+}
