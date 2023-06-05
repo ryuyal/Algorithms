aebe979cbc328f733b75b1dba37732bdfa3e9a4b
ryuyal <helloliu777@163.com>
2023-06-05 23:57:22 +0800

    leetcode 9 is done

---
+++ ForPractice/leetcode_9.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Liu on 2023/6/5.
+//
+#include<iostream>
+
+using namespace std;
+
+bool isPalindrome(int x) {
+    if(x < 0)
+        return false;
+    int cur = 0;
+    int num = x;
+    while(num != 0) {
+        cur = cur * 10 + num % 10;
+        num /= 10;
+    }
+    return cur == x;
+}
+
+int main(){
+    int num;
+    cin >> num;
+
+    bool res = isPalindrome(num);
+    cout << res << endl;
+}
