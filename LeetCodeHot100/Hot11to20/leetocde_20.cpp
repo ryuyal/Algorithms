4d08b81c7f9b48cdc5f682070d1d38f76e7198bd
ryuyal <helloliu777@163.com>
2023-06-09 23:53:33 +0800

    leetcode 20 is done

--- LeetCodeHot100/Hot11to20/leetocde_20.cpp
+++ LeetCodeHot100/Hot11to20/leetocde_20.cpp
@@ -22,11 +22,40 @@ bool isValid(string s) {
     return istrue;
 }
 
+bool isMatch(char c1, char c2){
+    if ((c1 == '(' && c2 == ')')||(c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}')){
+        return true;
+    }
+    return false;
+}
+bool isLeft(char c){
+    if (c == '(' || c == '{' || c == '['){
+        return true;
+    }
+    return false;
+}
+bool isValid2(string s){
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (isLeft(s[i])){
+            st.push(s[i]);
+        }else{
+            if (st.empty() || (!isMatch(st.top(), s[i]))){
+                return false;
+            }else{
+                st.pop();
+            }
+        }
+    }
+    return st.empty();
+}
+
 int main(){
 
     string s;
     cin >> s;
-    bool res = isValid(s);
+    bool res = isValid2(s);
 
     cout << res << endl;
 
