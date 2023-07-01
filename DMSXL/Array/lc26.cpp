c16364d8af25c1795460425b75a291a0b9daf64d
ryuyal <helloliu777@163.com>
2023-07-01 23:52:11 +0800

    leetcode 26 is done!

---
+++ DMSXL/Array/lc26.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Liu on 2023/7/1.
+// Description: 
+//
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int removeDuplicates(vector<int> & nums){
+    if (nums.size() == 0){
+        return 0;
+    }
+    int pos = 0;
+    nums[pos++] = nums[0];
+
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i] != nums[pos-1]){
+            nums[pos++] = nums[i];
+        }
+    }
+    return pos;
+}
+
+int main() {
+    int n;
+    cout << "Please input the size of vector: " << endl;
+    cin >> n;
+
+    cout << "Please input elements of vector: " << endl;
+    vector<int> nums;
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        nums.push_back(val);
+    }
+
+
+    int res = removeDuplicates(nums);
+
+    cout << res ;
+}
