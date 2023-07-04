c6da6ef166b673079083e6b121eeb519d99b94a1
ryuyal <helloliu777@163.com>
2023-07-04 23:33:34 +0800

    leetcode 160 is done!

---
+++ DMSXL/LinkedList/lc160.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Liu on 2023/7/4.
+// Description: 同面试题0207
+//
+
+#include<iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode * next;
+
+    ListNode(){}
+    ListNode(int v):val(v), next(nullptr){}
+    ListNode(int v, ListNode * n):val(v), next(n){}
+};
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode * ha = headA;
+    ListNode * hb = headB;
+
+    while(ha != hb){
+        ha = ha != nullptr ? ha->next : headB;
+        hb = hb != nullptr ? hb->next : headA;
+    }
+
+    return ha;
+}
+
+
+int main(){
+
+}
