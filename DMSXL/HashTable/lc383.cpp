ab92c71448cfb583f80f2ef4c7bc3d1891fc81b6
ryuyal <helloliu777@163.com>
2023-07-05 23:31:36 +0800

    leetcode 383 is done!

---
+++ DMSXL/HashTable/lc383.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Liu on 2023/7/4.
+// Description: 
+//
+
+#include <iostream>
+#include<unordered_map>
+using namespace std;
+
+bool canConstruct(string ransomNote, string magazine) {
+    unordered_map<char, int> mMap;
+    for (char ch : magazine) {
+        mMap[ch]++;
+    }
+
+    for (char ch : ransomNote) {
+        if (mMap[ch] > 0){
+            mMap[ch]--;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string ran;
+    string mag;
+
+    cin >> ran;
+    cin >> mag;
+
+    bool res = canConstruct(ran, mag);
+    cout << res;
+}
