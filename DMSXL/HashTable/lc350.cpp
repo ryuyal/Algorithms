7084b80617bd28cee0067ce219cd969b0bcf7988
ryuyal <helloliu777@163.com>
2023-07-06 21:43:51 +0800

    leetcode 350 is done!

---
+++ DMSXL/HashTable/lc350.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Liu on 2023/7/6.
+// Description: 
+//
+
+#include <iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> map;
+
+    for(int val : nums1){
+        ++map[val];
+    }
+
+    vector<int> res;
+    for(int val : nums2){
+        if (map[val] > 0)
+        {
+            res.push_back(val);
+            --map[val];
+        }
+
+    }
+    return res;
+}
+
+int main() {
+
+}
