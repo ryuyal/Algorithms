658502c7fe97fd7399ccd81f750e617e8cfa55a9
ryuyal <helloliu777@163.com>
2023-07-11 00:02:18 +0800

    Jianzhi offer 58-2 is done!

---
+++ JianZhiOffer/offer58_2.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Liu on 2023/7/11.
+// Description: 
+//
+
+#include<iostream>
+using namespace std;
+
+void reverse(string & s, int start, int end){
+    int left = start, right = end;
+
+    while(left < right){
+        char ch = s[left];
+        s[left] = s[right];
+        s[right] = ch;
+        left++;
+        right--;
+    }
+
+}
+string reverseLeftWords(string s, int n) {
+    reverse(s, 0, s.size()-1);
+
+    reverse(s, 0, s.size()-n-1);
+    reverse(s, s.size()-n, s.size()-1);
+
+    return s;
+}
+
+int main(){
+
+    string s;
+    getline(cin, s);
+
+    int n;
+    cin >> n;
+    string res = reverseLeftWords(s, n);
+
+    cout << res << endl;
+
+}
