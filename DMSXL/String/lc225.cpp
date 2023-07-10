ca97492fd8fb3f2b9a2a044d6ac328dca8e7f96b
ryuyal <helloliu777@163.com>
2023-07-11 00:04:55 +0800

    Leetcode 225 is done!

---
+++ DMSXL/String/lc225.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Liu on 2023/7/11.
+// Description: 
+//
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+
+    MyStack() {
+    }
+
+    void push(int x) {
+        q1.push(x);
+    }
+
+    int pop() {
+        int size = q1.size();
+        for(int i = 0; i < size-1; i++){
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        int res = q1.front();
+        q1.pop();
+        q1 = q2;
+
+        while(!q2.empty()){
+            q2.pop();
+        }
+
+        return res;
+    }
+
+    int top() {
+        return q1.back();
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+int main(){
+
+}
