b4fbdc81e020973361807b6900a7041dd807b91a
ryuyal <helloliu777@163.com>
2023-07-19 23:23:25 +0800

    leetcode 112 : hasPathSum

---
+++ DMSXL/Binary_Tree/lc112.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Liu on 2023/7/19.
+// Description: 
+//
+
+#include<iostream>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+bool hasPathSum(TreeNode* root, int targetSum) {
+    if (root == nullptr) {
+        return false;
+    }
+
+    if (root->left == nullptr && root->right == nullptr && targetSum == root->val) {
+        return true;
+    }
+
+    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+}
+
+
+int main(){
+
+}
