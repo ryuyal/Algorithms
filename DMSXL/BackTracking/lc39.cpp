cf952e85c2cd4c9d75a97d94d40fe02b06cf724e
ryuyal <helloliu777@163.com>
2023-07-27 23:44:15 +0800

    backtracking - combination

---
+++ DMSXL/BackTracking/lc39.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Liu on 2023/7/27.
+// Description: 
+//
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> path;
+int sum = 0;
+
+void backtracking(vector<int> & candidates, int target, int startIndex){
+    if(sum > target){
+        return ;
+    }
+
+    if(sum == target){
+        result.push_back(path);
+        return ;
+    }
+
+    for(int i = startIndex; i < candidates.size(); ++i){
+        sum += candidates[i];
+        path.push_back(candidates[i]);
+        backtracking(candidates, target, i);
+        sum -= candidates[i];
+        path.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum(vector<int> & candidates, int target){
+    backtracking(candidates, target, 0);
+
+    return result;
+}
+
+
+int main() {
+
+}
---
+++ DMSXL/BackTracking/lc40.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Liu on 2023/7/27.
+// Description: 
+//
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> path;
+int sum = 0;
+
+void backtracking(vector<int> & candidates, int target, int startIndex){
+    if(sum > target){
+        return ;
+    }
+
+    if(sum == target){
+        result.push_back(path);
+        return;
+    }
+
+    for(int i = startIndex; i < candidates.size(); ++i){
+        //  // used[i - 1] == true，说明同一树枝candidates[i - 1]使用过
+        //     // used[i - 1] == false，说明同一树层candidates[i - 1]使用过
+        //     // 要对同一树层使用过的元素进行跳过
+        // if(i > 0 && candidates[i] == candidates[i-1] && used[i-1]==false){
+        //     continue;
+        // }
+        // 要对同一树层使用过的元素进行跳过
+        if (i > startIndex && candidates[i] == candidates[i - 1]) {
+            continue;
+        }
+        sum += candidates[i];
+        path.push_back(candidates[i]);
+        // used[i] = true;
+        backtracking(candidates, target, i+1);
+        sum -= candidates[i];
+        // used[i] = false;
+        path.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum2(vector<int> & candidates, int target){
+    sort(candidates.begin(), candidates.end());
+
+    // backtracking(candidates, target, 0, used);
+    backtracking(candidates, target, 0);
+    return result;
+}
+
+int main() {
+
+}
