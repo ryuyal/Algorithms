3fb444a98f05c80d1ff42e201de76f99a007dc11
ryuyal <helloliu777@163.com>
2023-07-29 23:55:05 +0800

    dp - min cost climb stairs

---
+++ DMSXL/DynamicProgramming/lc746.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Liu on 2023/7/29.
+// Description: 
+//
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int minCostClimbingStairs(vector<int>& cost) {
+    vector<int> dp(cost.size()+1);
+
+    dp[0] = 0;
+    dp[1] = cost[1];
+
+    for(int i = 2; i <= cost.size(); ++i){
+        dp[i] =  min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+    }
+
+    return dp[cost.size()];
+}
+
+int main() {
+
+    vector<int> cost = {1,100,1,1,1,100,1,1,100,1};
+
+    int res = minCostClimbingStairs(cost);
+
+    cout << res << endl;
+
+}
