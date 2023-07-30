da6021134d5cbb737fa58ed88ae729a5d31e45c0
ryuyal <helloliu777@163.com>
2023-07-30 23:37:34 +0800

    dp - update

--- DMSXL/DynamicProgramming/lc746.cpp
+++ DMSXL/DynamicProgramming/lc746.cpp
@@ -10,9 +10,14 @@ using namespace std;
 int minCostClimbingStairs(vector<int>& cost) {
     vector<int> dp(cost.size()+1);
 
+    // dp[i]中的i代表登上第i级台阶，最终i=cost.size()
     dp[0] = 0;
-    dp[1] = cost[1];
+    dp[1] = 0;
 
+    /*
+     * dp[i - 1] 跳到 dp[i] 需要花费 dp[i - 1] + cost[i - 1]
+     * dp[i - 2] 跳到 dp[i] 需要花费 dp[i - 2] + cost[i - 2]
+     */
     for(int i = 2; i <= cost.size(); ++i){
         dp[i] =  min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
     }
