e9d493c56c5d6867d0363201bb917a6d2987cb3a
ryuyal <1020041116@njupt.edu.cn>
2023-08-23 17:26:42 +0800

    update lc76

--- DMSXL/Array/lc76.cpp
+++ DMSXL/Array/lc76.cpp
@@ -8,32 +8,32 @@
 #include<unordered_map>
 using namespace std;
 
-string minWindow(string s, string t) {
-    unordered_map<char, int> tmap, smap;
-    int left = 0, correct = 0;
-    string res = s + "initialize a max string";
-    // 对于t制作字符出现数的字典
-    for (auto item : t)
-        ++tmap[item];
-
-    for (int right = 0; right < s.size(); ++right) {
-        // smap维护的是当前窗口内的字符出现数的字典
-        ++smap[s[right]];
-        // 当前right对应s的字符是在t中出现的，并且数量上还没有达到冗余，是一次有效添加
-        if (tmap[s[right]] >= smap[s[right]])
-            ++correct;
-        // 字符串最短是空串 && 如果left对应的字符是冗余，那么进行右移删除
-        while (left < right && smap[s[left]] > tmap[s[left]])
-            --smap[s[left++]];
-        if (correct == t.size()){
-            // 窗口内已经满足t串的所有字符
-            if (right - left + 1 < res.size())
-                res = s.substr(left, right - left + 1);
-        }
-
-    }
-    return res == s + "initialize a max string" ? "" : res;
-}
+//string minWindow(string s, string t) {
+//    unordered_map<char, int> tmap, smap;
+//    int left = 0, correct = 0;
+//    string res = s + "initialize a max string";
+//    // 对于t制作字符出现数的字典
+//    for (auto item : t)
+//        ++tmap[item];
+//
+//    for (int right = 0; right < s.size(); ++right) {
+//        // smap维护的是当前窗口内的字符出现数的字典
+//        ++smap[s[right]];
+//        // 当前right对应s的字符是在t中出现的，并且数量上还没有达到冗余，是一次有效添加
+//        if (tmap[s[right]] >= smap[s[right]])
+//            ++correct;
+//        // 字符串最短是空串 && 如果left对应的字符是冗余，那么进行右移删除
+//        while (left < right && smap[s[left]] > tmap[s[left]])
+//            --smap[s[left++]];
+//        if (correct == t.size()){
+//            // 窗口内已经满足t串的所有字符
+//            if (right - left + 1 < res.size())
+//                res = s.substr(left, right - left + 1);
+//        }
+//
+//    }
+//    return res == s + "initialize a max string" ? "" : res;
+//}
 string minWindow(string s, string t) {
     unordered_map<char, int > hs, ht;
 
@@ -54,6 +54,8 @@ string minWindow(string s, string t) {
             ++cnt; // 新加入的字符s[right]必需，则cnt++。
         }
 
+        // 注意！！！
+        // 这里收缩完窗口时，最坏的情况是hs[left] == ht[s[left]] 所以不需要对cnt进行额外处理
         while(left < right && hs[s[left]] > ht[s[left]]){ // 满足条件
             --hs[s[left++]]; // 压缩窗口
         }
