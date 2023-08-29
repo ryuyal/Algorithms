ea0c6343354e710437e30cca762b945a0f380a95
ryuyal <1020041116@njupt.edu.cn>
2023-08-29 13:24:55 +0800

    update lc142

--- DMSXL/LinkedList/lc142.cpp
+++ DMSXL/LinkedList/lc142.cpp
@@ -23,7 +23,7 @@ ListNode *detectCycle(ListNode *head) {
 
 //    https://leetcode.cn/problems/linked-list-cycle-ii/solutions/12616/linked-list-cycle-ii-kuai-man-zhi-zhen-shuang-zhi-/
 /*
- * 设链表共有 a+ba+ba+b 个节点，其中 链表头部到链表入口 有 a 个节点（不计链表入口节点）， 链表环有 b 个节点（这里需要注意，a 和 b 是未知数，设两指针分别走了 f，s 步，则有：
+ * 设链表共有 a+b 个节点，其中 链表头部到链表入口 有 a 个节点（不计链表入口节点）， 链表环有 b 个节点（这里需要注意，a 和 b 是未知数，设两指针分别走了 f，s 步，则有：
 fast 走的步数是 slow 步数的 2 倍，即 f=2s；（解析： fast 每轮走 2 步）
 fast 比 slow 多走了 n 个环的长度，即 f=s+nb；（ 解析： 双指针都走过 a 步，然后在环内绕圈直到重合，重合时 fast 比 slow 多走 环的长度整数倍 ）。
 将以上两式相减得到 f=2nb，s=nb，即 fast 和 slow 指针分别走了 2n，n个环的周长。
