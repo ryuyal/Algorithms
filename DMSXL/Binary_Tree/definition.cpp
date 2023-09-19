86e1c16a71e069f064055ef9f634338aab1b08e3
ryuyal <1020041116@njupt.edu.cn>
2023-09-19 14:44:43 +0800

    treenode definition.

---
+++ DMSXL/Binary_Tree/definition.cpp
@@ -0,0 +1,20 @@
+
+//
+// Created by Yao on 2023/9/19.
+// Description:     
+//
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(){}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+};
+
+int main() {
+
+}
