eb0638a0cd29b22cd3ebf0562fed7a9d78f277b6
ryuyal <1020041116@njupt.edu.cn>
2023-09-20 14:14:55 +0800

    update lc222.

--- DMSXL/Binary_Tree/lc222.cpp
+++ DMSXL/Binary_Tree/lc222.cpp
@@ -16,6 +16,8 @@ struct TreeNode{
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// 普通二叉树 计算节点个数
+// 时间复杂度O(n) 空间复杂度O(logn)
 int countNodes(TreeNode* root) {
     if(root == nullptr){
         return 0;
@@ -25,6 +27,33 @@ int countNodes(TreeNode* root) {
     }
 
     return 1+countNodes(root->left) + countNodes(root->right);
+}
+
+// 完全二叉树
+int countNodes2(TreeNode * root){
+    if(root == nullptr){
+        return 0;
+    }
+
+    // 判断向左遍历的深度和向右遍历的深度是否相等, 相等说明是满二叉树
+    TreeNode * leftNode = root->left;
+    TreeNode * rightNode = root->right;
+
+    int leftDepth = 0, rightDepth = 0;
+    while(leftNode){
+        leftNode = leftNode->left;
+        leftDepth++;
+    }
+    while(rightNode){
+        rightNode = rightNode->right;
+        rightDepth++;
+    }
+
+    if(leftDepth == rightDepth){
+        return (2<<leftDepth) - 1;
+    }
+
+    return countNodes2(root->left) + countNodes2(root->right) + 1;
 }
 
 int main(){
