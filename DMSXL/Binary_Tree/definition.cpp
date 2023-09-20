c6bd81d03274d11095e8124b96b0f416cc86c822
ryuyal <1020041116@njupt.edu.cn>
2023-09-20 09:36:30 +0800

    update definition of tree node.

--- DMSXL/Binary_Tree/definition.cpp
+++ DMSXL/Binary_Tree/definition.cpp
@@ -5,14 +5,31 @@
 //
 
 #include <iostream>
+#include<vector>
 using namespace std;
 
+/*
+ * 二叉树节点
+ */
 struct TreeNode{
     int val;
     TreeNode * left;
     TreeNode * right;
     TreeNode(){}
     TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+};
+
+/*
+ * N 叉树节点
+ */
+struct Node{
+    int val;
+    vector<Node *> children;
+    Node(){}
+
+    Node(int _val):val(_val){}
+
+    Node(int _val, vector<Node*> _children):val(_val), children(_children){}
 };
 
 int main() {
