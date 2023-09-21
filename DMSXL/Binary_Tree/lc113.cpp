7771c6f58d5e99aeb11a95de87f0a1d2817fad9d
ryuyal <1020041116@njupt.edu.cn>
2023-09-21 09:30:49 +0800

    lc113 is done!

--- DMSXL/Binary_Tree/lc113.cpp
+++ DMSXL/Binary_Tree/lc113.cpp
@@ -18,29 +18,36 @@ struct TreeNode{
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// preorder dfs
 void traversal(TreeNode * root, vector<vector<int>> & res, vector<int> & path, int targetSum){
     if(root == nullptr){
         return ;
     }
 
     path.push_back(root->val);
-    if(root->left == nullptr && root->right == nullptr && targetSum == 0){
+    if(root->left == nullptr && root->right == nullptr && targetSum-root->val == 0){ // 说明当前root节点为叶子节点 且节点值符合要求
         res.push_back(path);
+        return ;
     }
 
     if(root->left != nullptr){
         traversal(root->left, res, path, targetSum-root->val);
+        path.pop_back();
     }
 
     if(root->right != nullptr){
         traversal(root->right, res, path, targetSum-root->val);
+        path.pop_back();
     }
-    path.pop_back();
 }
 
 vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
     vector<vector<int>> res;
     vector<int> path;
+
+    if(root == nullptr){
+        return res;
+    }
 
     traversal(root, res, path, targetSum);
 
