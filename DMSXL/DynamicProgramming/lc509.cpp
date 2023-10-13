6eaa8789821e998ecafc694b6c9a3fef139b8e9b
ryuyal <1020041116@njupt.edu.cn>
2023-10-13 15:05:05 +0800

    update lc509

--- DMSXL/DynamicProgramming/lc509.cpp
+++ DMSXL/DynamicProgramming/lc509.cpp
@@ -24,6 +24,23 @@ int fib(int n){
     return res;
 }
 
+int fib1(int n) {
+
+
+    if(n == 0 || n == 1){
+        return n;
+    }
+    int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i = 2; i <= n; ++i){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
+
 int fib2(int n){
     if(n < 2){
         return n;
@@ -34,5 +51,6 @@ int fib2(int n){
 
 int main() {
     cout << fib(5) << endl;
+    cout << fib(3) << endl;
     cout << fib2(5) << endl;
 }
