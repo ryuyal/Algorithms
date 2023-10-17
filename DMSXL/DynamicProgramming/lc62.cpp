6462abd4230e2aa8b1029a2b801c4abe238e79a9
ryuyal <1020041116@njupt.edu.cn>
2023-10-17 08:50:45 +0800

    lc 62 is done.

--- DMSXL/DynamicProgramming/lc62.cpp
+++ DMSXL/DynamicProgramming/lc62.cpp
@@ -8,13 +8,7 @@
 using namespace std;
 
 int uniquePaths(int m, int n) {
-    int dp[m][n];
-
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            dp[i][j] = 0;
-        }
-    }
+    vector<vector<int>> dp(m, vector<int>(n, 0));
 
     for(int i = 1; i < n; ++i){
         dp[0][i] = 1;
